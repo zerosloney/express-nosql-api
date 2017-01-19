572803b32f18041517f382191108086a0b60a5da
daichen <daichen1017@vip.qq.com>
2017-01-19 15:25:33 +0800

    delete

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home listing. */
-router.get('/', function(req, res, next) {
-    res.json({ error: '0', msg: 'welcome to use api' });
-});
-
-module.exports = router;
